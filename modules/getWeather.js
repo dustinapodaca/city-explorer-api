d8748165a740b6be23bc9e4622e417978c49ef24
dustinapodaca <dustinapodaca@gmail.com>
2022-09-29 19:59:10 -0600

    finished modularizing

--- modules/getWeather.js
+++ modules/getWeather.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 
 async function getWeather(req, res) {
   const cityLocation = req.query.searchQuery; // note to use searchQuery on front end.
-  const url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${cityLocation}&key=${process.env.WEATHER_API_KEY}`;
+  const url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${cityLocation}&key=${process.env.WEATHER_API_KEY}&days=6`;
   console.log(url);
   try {
     const weatherRes = await axios.get(url);
@@ -14,7 +14,10 @@ async function getWeather(req, res) {
     weatherRes.data.data.forEach(day => {
       let date = day.datetime;
       let description = day.weather.description;
-      weatherArray.push(new Forecast(date, description));
+      let low_temp = day.low_temp;
+      let high_temp = day.high_temp;
+      let city_name = weatherRes.data.city_name;
+      weatherArray.push(new Forecast(date, description, low_temp, high_temp, city_name));
     });
     res.status(200).send(weatherArray);
 
@@ -24,9 +27,13 @@ async function getWeather(req, res) {
 }
 
 class Forecast {
-  constructor(date, description) {
+  constructor(date, description, low_temp, high_temp, city_name) {
     this.date = date;
     this.description = description;
+    this.low_temp = low_temp;
+    this.high_temp = high_temp;
+    this.city_name = city_name;
+
   }
 }
 
